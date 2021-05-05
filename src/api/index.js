d710b37d81e1a76751bcc480d566720ff146ee74
taurusilver7 <prasad20697@gmail.com>
2021-05-05 12:55:05 +0530

    axios & api_url imported

---
+++ src/api/index.js
@@ -0,0 +1,6 @@
+// API calls to the back-end routes are created & sent from here.
+// The actions are dispatched using redux-dispatch
+
+import axios from "axios";
+
+const url = "http://localhost:5000/posts";
